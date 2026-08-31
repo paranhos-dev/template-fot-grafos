0c39763d9320b73d884f88b3a8d77cf358174f50
BERNARDO PARANHOS BORGES OLIVEIRA <beparanhosborges@gmail.com>
2026-08-31 10:20:57 -0300

    chore: atualiza Vite para 8.2.2 e corrige vulnerabilidades do esbuild

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+// base = nome do repositorio, necessario para o GitHub Pages de projeto
+// (o site fica em https://paranhos-dev.github.io/template-fot-grafos/)
+export default defineConfig({
+  base: '/template-fot-grafos/',
+})
